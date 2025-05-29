d5f16dc61a583b56bfb0ca1f1f861b725e878d0b
Juansemu40 <Juan.munozcai@campusucc.edu.co>
2025-05-29 04:24:43 +0000

    Se hicieron correctamente peticiones a la bd y se hizo correctemnte la conexion a la bd

--- PracticaDOO/src/database/db.js
+++ PracticaDOO/src/database/db.js
@@ -2,9 +2,18 @@
 module.exports = {
     /*
     db: "mongodb+srv://ciclo3:ciclo3@cluster0.a6x9d.mongodb.net/utp_grupo2?retryWrites=true&w=majority"
+
+
+
+    
     */
 
-    db: "mongodb+srv://admin:admin@cluster0.u1dtbte.mongodb.net/Persona?retryWrites=true&w=majority"
+ 
+    
+db: "mongodb+srv://admin:admin@cluster0.u1dtbte.mongodb.net/Persona?retryWrites=true&w=majority"
+
+
+
 
 }
 
